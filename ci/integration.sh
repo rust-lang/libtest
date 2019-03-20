42760f5c59be7280c1430a5890921901b764cfaa
gnzlbg <gonzalobg88@gmail.com>
2019-03-20 10:48:11 +0100

    Allow the integration test to fail

--- ci/integration.sh
+++ ci/integration.sh
@@ -7,7 +7,8 @@ rm -r compiletest-rs || true
 git clone git@github.com:laumann/compiletest-rs.git
 (
     cd compiletest-rs
-    sed -i '' 's@libtest = "0.0.1"@libtest = { path = "..", features = [ "unstable" ] }@g' Cargo.toml
+    sed -i '' 's@libtest = "0.0.2"@libtest = { path = "..", features = [ "unstable" ] }@g' Cargo.toml
     echo "[workspace]" >> Cargo.toml
     cargo build
+    cargo build --features=unstable
 )
